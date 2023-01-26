c9b89b165cc42d7471d57723404860e4380c6528
Alok3333 <alokprajapati697@gmail.com>
2023-01-26 17:49:42 +0530

    Api setUp is done

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');             //Corss-Origin Resourse Sharing
 const connectDB = require('./mongodb');
+const todoRoute = require('./routes/todoRoute');
 
 const app = express();          // Calling the express function
 
@@ -8,6 +9,8 @@ app.use(express.json());
 app.use(cors());
 
 connectDB();
+
+app.use('/api', todoRoute);
 
 app.listen(6000, () => {
     console.log("Server is running on port 6000");
