4478d96d3f54a3905157376eabc11bee16e62e67
Alok3333 <alokprajapati697@gmail.com>
2023-01-26 18:33:00 +0530

    created frontend and setup reactjs

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const cors = require('cors');             //Corss-Origin Resourse Sharing
+const cors = require('cors'); //Corss-Origin Resourse Sharing
 const connectDB = require('./mongodb');
 const todoRoute = require('./routes/todoRoute');
 
-const app = express();          // Calling the express function
+const app = express(); // Calling the express function
 
 app.use(express.json());
 app.use(cors());
@@ -13,5 +13,5 @@ connectDB();
 app.use('/api', todoRoute);
 
 app.listen(6000, () => {
-    console.log("Server is running on port 6000");
-})
+  console.log('Server is running on port 6000');
+});
